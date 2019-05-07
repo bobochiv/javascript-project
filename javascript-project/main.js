3588f12af1c65f46d1f000670bc682b1f5aef8db
Bobo <bobochiv@gmail.com>
2019-05-07 18:57:40 -0400

    update

--- javascript-project/main.js
+++ javascript-project/main.js
@@ -27,6 +27,7 @@ const GAME_STEPS = ['SETUP_PLAYER', 'SETUP_BOARD', 'GAME_START'];
 let gameStep = 0; // The current game step, value is index of the GAME_STEPS array.
 // The board holds all the game entities. It is a 2D array.
 let player = {}; // The player object
+let board = [];
 
 // Utility function to print messages with different colors. Usage: print('hello', 'red');
 function print(arg, color) {
@@ -48,7 +49,7 @@ function assertEqual(obj1, obj2) {}
 
 // Clones an array of objects
 // returns a new array of cloned objects. Useful to clone an array of item objects
-function cloneArray(objs) {}
+// function cloneArray(objs) {}
 
 // Uses a player item (note: this consumes the item, need to remove it after use)
 // itemName is a string, target is an entity (i.e. monster, tradesman, player, dungeon)
@@ -65,19 +66,17 @@ function useSkill(skillName, target) {}
 // First and last rows are walls
 // First and last columns are walls
 // All the other entities are grass entities
-function createBoard(rows, columns) {}
+//function createBoard(rows, columns) {}
 
 // Updates the board by setting the entity at the entity position
 // An entity has a position property, each board cell is an object with an entity property holding a reference to the entity at that position
 // When a player is on a board cell, the board cell keeps the current entity property (e.g. monster entity at that position) and may need to have an additional property to know the player is there too.
-function updateBoard(entity) {}
+// function updateBoard(entity) {}
 
 // Sets the position property of the player object to be in the middle of the board
 // You may need to use Math methods such as Math.floor()
-function placePlayer() {}
 
 // Creates the board and places player
-function initBoard(rows, columns) {}
 
 // Prints the board
 
@@ -116,6 +115,199 @@ function move(direction) {}
 function setupPlayer() {
   printSectionTitle('SETUP PLAYER');
   print(
+    "`Please create` a player using the createPlayer function. Usage: createPlayer('Bob')"
+  );
+  print(
+    "You can optionally pass in a level and items, e.g. createPlayer('Bob', 3, [items[0], items[2]]). items[0] refers to the first item in the items variable"
+  );
+  print("Once you're done, go to the next step with next()");
+}
+setupPlayer();
+
+function createPlayer(name, level, items = []) {
+  console.log(
+    'Created player with name ' + name,
+    'and level ' + level,
+    'with items ' + items
+  );
+  player = {
+    name,
+    level,
+    items,
+    hp: 100,
+    attack: 10,
+    exp: 0,
+    gold: 0,
+    getExpToLevel: function() {
+      return this.level * 20;
+    },
+    getMaxHp: function() {
+      return this.level * 100;
+    },
+    speed: 3000,
+    type: 'player',
+    position: {},
+    //levelUp:
+  };
+}
+function updateBoard(entity) {
+  board[entity.position.row][entity.position.column] = { entity };
+}
+
+function createMonster(level, items = [], position) {
+  console.log('Creating monster...');
+  return {
+    name: monsterNames[Math.floor(Math.random(monsterNames.length) + 0)],
+    level,
+    items,
+    hp: level * 100,
+    attack: level * 10,
+    exp: 0,
+    gold: 0,
+    items,
+    getExp: function() {
+      return this.level * 10;
+    },
+    getMaxHp: function() {
+      return this.level * 100;
+    },
+    speed: 6000 / level,
+    type: 'monster',
+    position,
+    //levelUp:
+  };
+}
+function createTradesman(items, position) {
+  console.log('Creating tradesman...');
+  return {
+    name: 'Mercenary',
+    hp: Infinity,
+    items,
+    position,
+    type: 'tradesman',
+    //getMaxHp
+  };
+}
+
+let playerAttack, enemyAttack;
+function startBattle(enemy) {
+  console.log('Encountered a Monster!');
+}
+
+function shopTrade() {
+  console.log("Hey that's the tradesman!");
+}
+
+//BOARD
+
+function createBoard(rows, columns) {
+  board = [];
+  for (let i = 0; i < rows; i++) {
+    let row = [];
+    for (let j = 0; j < columns; j++) {
+      if (i === 0 || i === rows - 1 || j === 0 || j === columns - 1) {
+        row.push({ entity: { type: 'wall', position: { row: i, column: j } } });
+      } else
+        row.push({
+          entity: { type: 'grass', position: { row: i, column: j } },
+        });
+    }
+    board.push(row);
+  }
+}
+function getChar(type) {
+  if (type === 'grass') return '.';
+  if (type === 'wall') return '#';
+  if (type === 'monster') return 'M';
+  if (type === 'tradesman') return 'T';
+  return type.charAt(0).toUpperCase();
+}
+
+function printBoard() {
+  for (let i = 0; i < board.length; i++) {
+    let row = '';
+    for (let j = 0; j < board[i].length; j++) {
+      row += board[i][j].player ? 'P' : getChar(board[i][j].entity.type);
+    }
+    print(row);
+  }
+}
+
+function placePlayer() {
+  const newCell = {
+    row: Math.floor(board.length / 2),
+    column: Math.floor(board[0].length / 2),
+  };
+  player.position = newCell;
+  updatePlayerCell(newCell);
+}
+
+function updatePlayerCell(newCell) {
+  ``;
+  board[player.position.row][player.position.column] = {
+    entity: getCell(player.position).entity,
+  };
+  board[newCell.row][newCell.column] = {
+    entity: getCell(newCell).entity,
+    player,
+  };
+  player.position = newCell;
+}
+
+function initBoard(rows, columns) {
+  console.log('Creating board and placing player...');
+  printBoard();
+  createBoard(rows, columns);
+  placePlayer();
+}
+function move(direction) {
+  switch (direction) {
+    case 'U':
+      moveCell({
+        row: player.position.row - 1,
+        column: player.position.column,
+      });
+      break;
+    case 'D':
+      moveCell({
+        row: player.position.row + 1,
+        column: player.position.column,
+      });
+      break;
+    case 'L':
+      moveCell({
+        row: player.position.row,
+        column: player.position.column - 1,
+      });
+      break;
+    case 'R':
+      moveCell({
+        row: player.position.row,
+        column: player.position.column + 1,
+      });
+      break;
+  }
+  printBoard();
+}
+
+function getCell(position) {
+  return board[position.row][position.column];
+}
+function moveCell(newCell) {
+  const entity = getCell(newCell).entity;
+  if (entity.type === 'wall') {
+    print('you bumped into a wall !!!');
+  } else {
+    updatePlayerCell(newCell);
+    if (entity.type === 'monster') startBattle(entity);
+    if (entity.type === 'item') pickupItem(entity);
+    if (entity.type === 'tradesman') shopTrade(entity);
+  }
+}
+
+function setupPlayer() {
+  printSectionTitle('SETUP PLAYER');
+  print(
     "Please create a player using the createPlayer function. Usage: createPlayer('Bob')"
   );
   print(
@@ -131,6 +323,10 @@ function setupBoard() {
     'Setup monsters, items and more using createMonster(attr), createItem(item, pos), createTradesman(items, pos), createDungeon(pos), updateBoard(entity)'
   );
   print("Once you're done, go to the next step with next()");
+  initBoard(7, 15);
+  updateBoard(createMonster(1, 1, { row: 2, column: 5 }));
+  updateBoard(createTradesman(1, { row: 3, column: 5 }));
+  next();
 }
 
 function startGame() {
@@ -168,139 +364,4 @@ function run() {
 print('Welcome to the game!', 'gold');
 print('Follow the instructions to setup your game and start playing');
 
-function setupPlayer() {
-  printSectionTitle('SETUP PLAYER');
-  print(
-    "`Please create` a player using the createPlayer function. Usage: createPlayer('Bob')"
-  );
-  print(
-    "You can optionally pass in a level and items, e.g. createPlayer('Bob', 3, [items[0], items[2]]). items[0] refers to the first item in the items variable"
-  );
-  print("Once you're done, go to the next step with next()");
-}
-
-setupPlayer();
-
-function createPlayer(name, level, items = [{ type: 'item', position: {} }]) {
-  console.log(
-    'Created player with name ' + name,
-    'and level ' + level,
-    'with items ' + items
-  );
-  player = {
-    name,
-    level,
-    items,
-    hp: 100,
-    attack: 10,
-    exp: 0,
-    gold: 0,
-    items,
-    //getExpToLevel:,
-    //getMaxHp:,
-    //skills: (2) [{...},{...}]
-    speed: 3000,
-    type: 'player',
-    position: {},
-    //levelUp:
-  };
-}
-function createMonster(level, items, position) {
-  name = monsterNames[Math.floor(Math.random() * monsterNames.length)];
-  monster = {
-    name,
-    level,
-    items,
-    hp: level * 100,
-    attack: level * 10,
-    exp: 0,
-    gold: 0,
-    items,
-    // getExp,
-    //getMaxHp:,]
-    speed: 6000 / level,
-    type: 'monster',
-    position: {},
-    //levelUp:
-  };
-}
-function createTradesman(items, position) {
-  tradesman = {
-    name: 'Mercenary',
-    hp: 99999,
-    items: [{}],
-    position: {},
-    type: 'tradesman',
-    //getMaxHp
-  };
-}
-
-//BOARD
-
-let board = [];
-function initBoard(rows, cols) {
-  for (let i = 0; i < rows; i++) {
-    board.push([]);
-    for (let j = 0; j < cols; j++) {
-      if (i === 0 || i === rows - 1 || j === 0 || j === cols - 1) {
-        board[i].push({ type: 'wall', position: { row: i, column: j } });
-      } else if (i === rows / 2 && j === cols / 2) {
-        board[i].push({ type: 'player', position: { row: i, column: j } });
-      } else {
-        board[i].push({ type: 'grass', position: { row: i, column: j } });
-      }
-    }
-  }
-  console.log('Creating board and placing player...');
-  printBoard();
-}
-
-function printBoard() {
-  let rows = board.length;
-  let cols = board[0].length;
-  str = '';
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      let currentType = board[i][j].type;
-      if (j === cols - 1) {
-        str = str.concat('#\n');
-      } else if (currentType === 'wall') {
-        str = str.concat('#');
-      } else if (currentType === 'grass') {
-        str = str.concat('.');
-      } else if (currentType === 'player') {
-        str = str.concat('P');
-        board[i].push({ player, position: { row: i, column: j } });
-      }
-    }
-  }
-  placePlayer();
-  console.log(str);
-}
-
-function move(direction) {
-  let i = board.length;
-  let j = board[0].length;
-      if ((direction = 'U')) {
-        board[i].push({ player, position: { row: i - 1, column: j } });
-      } else if ((direction = 'D')) {
-        board[i].push({ player, position: { row: i + 1, column: j } });
-      } else if ((direction = 'L')) {
-        board[j].push({ player, position: { row: i, column: j - 1 } });
-      } else if ((direction = 'R')) {
-        board[j].push({ player, position: { row: i, column: j + 1 } });
-      }
-    }
-    move('U');
-  }
-}
-// function placePlayer() {
-//   let rows = board.length;
-//   let cols = board[0].length;
-//   for (let i = 0; i < rows; i++) {
-//     for (let j = 0; j < cols; j++) {
-//       if (i === rows / 2 && j === cols / 2) {
-//       }
-//     }
-//   }
-// }
+run();
